c62e4accc754f63107129af2751bd4b05f7f905e
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-15 09:51:46 +0300

    Deleted font import from Chat.css. Redesing title of message

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -11,7 +11,7 @@ const Chat = () => {
             <img src={icon} alt=""></img>
           </div>
           <div className="text">
-            <h3>Ivanchik</h3>
+            <h3>&lt; Ivanchik /&gt;</h3>
             <p>
               I`m 16 years old, I`ve been putting me soul into a world of
               web-development for two whole years now
@@ -23,7 +23,7 @@ const Chat = () => {
             <img src={icon} alt=""></img>
           </div>
           <div className="text">
-            <h3>Ivanchik</h3>
+            <h3>&lt; Ivanchik /&gt;</h3>
             <p>
               I spent the first year learning to code with my friend, ready to
               explore the world of code further! 💻🚀
