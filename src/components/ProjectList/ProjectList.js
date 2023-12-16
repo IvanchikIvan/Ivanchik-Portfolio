b5768c08e2fc47de2d0aaecdc22c1de39475e849
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-16 23:27:52 +0300

    Added animation to projects list

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ProjectList.css";
 import standswiki from "../Assets/standswiki.png";
-import gist_site_icon from '../Assets/gift-site.png'
+import gist_site_icon from "../Assets/gift-site.png";
+import { animated, useInView } from "react-spring";
 
 const projects = [
   {
     title: "Site-Gift for girlfriend",
-    description:
-      "",
+    description: "",
     image: gist_site_icon,
     link: "https://example.com/project1",
   },
@@ -20,18 +20,34 @@ const projects = [
 ];
 
 const Portfolio = () => {
+  const [projectRef, projectStyles] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        x: -200,
+      },
+      to: {
+        opacity: 1,
+        x: 0,
+      },
+    }),
+    {
+      rootMargin: "-20% 0%",
+    }
+  );
+
   return (
     <div className="portfolio" id="projects">
       <h2>Projects</h2>
       <div className="projects">
         {projects.map((project, index) => (
-          <div className="project" key={index}>
+          <animated.div className="project" key={index} style={projectStyles} ref={projectRef}>
             <img src={project.image} alt={project.title} />
             <div className="project-info">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
             </div>
-          </div>
+          </animated.div>
         ))}
       </div>
     </div>
