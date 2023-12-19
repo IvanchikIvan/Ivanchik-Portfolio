a582133d6676ff8153f1149ddde1cf2006260012
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-19 23:43:49 +0300

    Rewritten class names to match the BEM in the Hero block

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -4,25 +4,25 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="columns">
-        <div className="rows">
-          <div className="frontend-developer">
+      <div className="hero__columns">
+        <div className="hero__rows">
+          <div className="hero__frontend-developer">
             <p>Frontend Developer</p>
           </div>
-          <div className="unleash-imagination-in-code">
+          <div className="hero__unleash-imagination-in-code">
             <p>Unleash Imagination in Code</p>
           </div>
-          <button className="contact-me">Contact Me</button>
+          <button className="hero__contact-me">Contact Me</button>
         </div>
-        <div className="grid">
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
+        <div className="hero__grid">
+          <div className="hero__grid-item"></div>
+          <div className="hero__grid-item"></div>
+          <div className="hero__grid-item"></div>
+          <div className="hero__grid-item"></div>
+          <div className="hero__grid-item"></div>
+          <div className="hero__grid-item"></div>
+          <div className="hero__grid-item"></div>
+          <div className="hero__grid-item"></div>
         </div>
       </div>
     </div>
