2b616c298ca67e96313192843c590b6e7b3b9aab
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-19 23:37:26 +0300

    Rewritten class names to match the BEM in the Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,10 +6,16 @@ const Footer = () => {
   return (
     <footer className="footer" id="footer">
       <div className="im-glad-to-answer-your-questions">
-        <div className="im-glad-to-answer-your-questions-image__container">
-          <img src={hello} className="im-glad-to-answer-your-questions__image"></img>
+        <div className="im-glad-to-answer-your-questions__image-container">
+          <img
+            src={hello}
+            className="im-glad-to-answer-your-questions__image"
+            alt="Hello Gif"
+          ></img>
         </div>
-        <p>E-mail me, I'm always in touch!</p>
+        <p className="im-glad-to-answer-your-questions__text">
+          E-mail me, I'm always in touch!
+        </p>
         <p className="made-by-Ivanchik">© 2023 — Made by Ivanchik</p>
       </div>
       <div className="contact-me-form-container">
@@ -17,6 +23,7 @@ const Footer = () => {
           <input
             type="text"
             id="name"
+            className="contact-me-form__input"
             placeholder="Name"
             name="name"
             value={null}
@@ -26,21 +33,25 @@ const Footer = () => {
           <input
             type="email"
             id="email"
+            className="contact-me-form__input"
             placeholder="Email"
             name="email"
             value={null}
             onChange={null}
             required
           />
-          <input
+          <textarea
             id="message"
             name="message"
+            className="contact-me-form__textarea"
             value={null}
             placeholder="Message"
             onChange={null}
             required
           />
-          <button type="submit">Contact Me</button>
+          <button type="submit" className="contact-me-form__button">
+            Contact Me
+          </button>
         </form>
       </div>
     </footer>
